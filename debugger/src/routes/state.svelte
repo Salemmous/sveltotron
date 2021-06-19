306cc348b74ceeb8a181ed1db8d809a18777adb8
Salemmous <thomas.debluts@gmail.com>
2021-06-19 23:26:35 +0300

    Add state tracking

---
+++ debugger/src/routes/state.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { stores } from '$lib/state';
+	import JSONTree from 'svelte-json-tree';
+</script>
+
+<section class="p-4">
+	<h1 class="text-blue-500">State</h1>
+	<div class="pt-8 w-full space-y-4">
+		{#each Object.values($stores) as store}
+			<div class="py-8 border-t border-gray-100 border-opacity-50 w-full">{store.name}</div>
+			{#each store.value as value}
+				<JSONTree {value} />
+			{/each}
+		{/each}
+	</div>
+</section>
